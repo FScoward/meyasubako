391b639990cf1e2836620ebadbc424259274c080
FScoward <endlick@gmail.com>
2020-12-02 22:52:13 +0900

    init

---
+++ src/main/kotlin/com/github/fscoward/meyasubako/MeyasubakoApplication.kt
@@ -0,0 +1,11 @@
+package com.github.fscoward.meyasubako
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MeyasubakoApplication
+
+fun main(args: Array<String>) {
+	runApplication<MeyasubakoApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/fscoward/meyasubako/MeyasubakoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.fscoward.meyasubako
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MeyasubakoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
